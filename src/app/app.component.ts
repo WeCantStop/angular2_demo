6374555d4ec56e23906ee2e2cadedc14e9c4ee05
Will <pan123033@qq.com>
2017-08-07 22:10:03 +0800

    选择英雄(click)='selectHero'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ export class Hero {
     name: string;
 }
 
+const HEROS: Hero[] = [
+  { id: 11, name: 'Mr.Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
     selector: 'app-root',
     // templateUrl: './app.component.html',
@@ -16,6 +29,18 @@ export class Hero {
                 <div><label>name:</label>
                     <input type="text" [(ngModel)]="hero.name" placeholder="name">
                 </div>
+                <ul>
+                  <li *ngFor="let hero of heros" (click)="onSelectHero(hero)" class="hero-item">
+                    <span>{{ hero.id }}</span>
+                    <span>{{ hero.name }}</span>
+                  </li>
+                </ul>
+                <div *ngIf="selectedHero">
+                  <div>
+                    <span>{{ selectedHero.id }}</span>
+                    <span>{{ selectedHero.name }}</span>
+                  </div>
+                </div>
                 `
 })
 export class AppComponent {
@@ -24,4 +49,9 @@ export class AppComponent {
         id: 1,
         name: '德玛西亚'
     };
+    heros = HEROS;
+    selectedHero: Hero;
+    onSelectHero(hero: Hero): void {
+      this.selectedHero = hero;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 /** form相关的东西需要引进此模块 **/
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
